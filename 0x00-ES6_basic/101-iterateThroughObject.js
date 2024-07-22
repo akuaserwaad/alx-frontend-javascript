0be4e02be4c757e3ef8da3bb959e90c92c9ac4a9
akuaserwaa93@gmail.com <141975577+akuaserwaad@users.noreply.github.com>
2024-07-22 15:15:51 -0400

    edits

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -2,3 +2,4 @@ export default function iterateThroughObject(reportWithIterator) {
     const employees = [...reportWithIterator];
     return employees.join(' | ');
   }
+  
--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -13,3 +13,4 @@ function getCurrentYear() {
   
     return budget;
   }
+  
