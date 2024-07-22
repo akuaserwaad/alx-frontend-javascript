4e5286b18c71d578866ab785c7ca8387ca3da31f
akuaserwaa93@gmail.com <141975577+akuaserwaad@users.noreply.github.com>
2024-07-22 19:21:59 -0400

    removed excess space

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,11 @@
+
 export default function appendToEachArrayValue(array, appendString) {
-    const wordsArray = [];
-  
-    for (const word of array) {
-      wordsArray.push(`${appendString}${word}`);
-    }
-  
-    return wordsArray;
+  const wordsArray = [];
+
+  for (const word of array) {
+    wordsArray.push(`${appendString}${word}`);
   }
-  
+
+  return wordsArray;
+}
+ 
