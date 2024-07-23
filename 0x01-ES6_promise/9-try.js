6047945ee598adec6c3c4bf7a114fbfa40120364
akuaserwaa93@gmail.com <141975577+akuaserwaad@users.noreply.github.com>
2024-07-23 18:01:26 -0400

    task 9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,19 @@
+/**
+ * Executes the given math function and pushes the result
+ * or an error message to a queue.
+ *
+ * @param {function} mathFunction - The function to be executed.
+ * @return {Array} An array containing the result or error message.
+ */
+export default function guardrail(mathFunction) {
+    const queue = [];
+  
+    try {
+      queue.push(mathFunction());
+    } catch (err) {
+      queue.push(err.toString());
+    } finally {
+      queue.push('Guardrail was processed');
+    }
+    return queue;
+  }
