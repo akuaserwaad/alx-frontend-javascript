7807b5c0983b7a5d2f26a322bde6d1ff95f74a37
akuaserwaa93@gmail.com <141975577+akuaserwaad@users.noreply.github.com>
2024-07-23 17:57:46 -0400

    task 4

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,14 @@
+/**
+ * Signs a User up with their first and last names.
+ *
+ * @param {string} firstName - The first name of the user.
+ * @param {string} lastName - The last name of the user.
+ * @return {Promise<Object>} A Promise that resolves with an
+ * object containing the provided first name and last name.
+ */
+export default function signUpUser(firstName, lastName) {
+    return Promise.resolve({
+      firstName,
+      lastName,
+    });
+  }
