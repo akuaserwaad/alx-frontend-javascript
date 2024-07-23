2e7f0d12710af9a56c2e76f9d2099defa08674a0
akuaserwaa93@gmail.com <141975577+akuaserwaad@users.noreply.github.com>
2024-07-23 18:10:23 -0400

    file changes made

--- 0x01-ES6_promise/.eslintrc.js
+++ 0x01-ES6_promise/.eslintrc.js
@@ -33,4 +33,13 @@ module.exports = {
       }
     ]
   };
+export default function getFullResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+        if (success) {
+            resolve({ status: 200, body: 'Success' });
+        } else {
+            reject(Error('The fake API is not working currently'));
+        }
+    });
+}
   
