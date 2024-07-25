f38ee69c5ac519727147c24fd89fb24a94495082
akuaserwaa93@gmail.com <141975577+akuaserwaad@users.noreply.github.com>
2024-07-24 23:57:53 -0400

    excess line removed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,23 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
+/**
+ * Handles the profile signup process asynchronously.
+ *
+ * @param {string} firstName - The first name of the user.
+ * @param {string} lastName - The last name of the user.
+ * @param {string} fileName - The name of the file.
+ * @return {Promise<Array<Object>>} A promise that resolves with an array
+ * of objects containing the status, value or error of each promise.
+ */
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = await Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((values) => {
-    const arr = [];
-    for (const item of values) {
-      arr.push({ status: item.status, value: item.value || item.reason });
-    }
-    return arr;
-  });
+  ]);
+
+  return results.map((res) => ({
+    status: res.status,
+    value: res.status === 'fulfilled' ? res.value : String(res.reason),
+  }));
 }
